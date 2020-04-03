e69acdd7e78b8cfdc986a3244d01e50451e2472a
nesrine messaoudi <nesrinemessaoudi270@gmail.com>
2020-04-03 17:41:09 +0100

    Migration de la base

--- database/migrations/2020_03_29_144801_create_customers_table.php
+++ database/migrations/2020_03_29_144801_create_customers_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
 
 class CreateCustomersTable extends Migration
 {
@@ -17,7 +18,7 @@ class CreateCustomersTable extends Migration
            
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email', 211)->unique();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('adress');
